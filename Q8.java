426a4a2e297052b8c31e834a0f764daaf09a2384
Lalit Tyagi <luckytyagi61@gmail.com>
2018-06-21 15:20:09 +0530

    Create Q8.java

---
+++ Q8.java
@@ -0,0 +1,27 @@
+/*
+
+You’re given an array. Print the elements of the array which are greater than its previous elements in the array.
+Input : 2 -3 -4 5 9 7 8    
+Output: 5 9 8
+You should solve this question in O(n) time.
+
+*/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=1;i<n;i++){
+		    if(arr[i]>arr[i-1]){
+		        System.out.println(arr[i]);
+		    }
+		}
+    }
+}
